ae18990390e5110e34db2356bf068bce982d7f15
adhiraj-code <alkarathore87@gmail.com>
2026-05-19 19:51:23 +0530

    Add files via upload

---
+++ Object-Oriented Programming/Class.py
@@ -0,0 +1,4 @@
+class fruits:
+    print("An apple a day, keeps the Doctor away!")
+
+apple = fruits()
---
+++ Object-Oriented Programming/Constructor.py
@@ -0,0 +1,8 @@
+class fruits:
+    def __init__(self, name, colour):
+        self.name = name
+        self.colour = colour
+
+apple = fruits("Apple", "Red")
+print(apple.name)
+print(apple.colour)
